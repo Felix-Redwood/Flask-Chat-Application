10fac2c20c6b00e198ac10451adfcd510622ab90
Ubuntu <ubuntu@ip-172-31-80-83.ec2.internal>
2020-04-02 12:53:00 +0000

    linked to chat.html

--- run.py
+++ run.py
@@ -9,11 +9,8 @@ messages = []
 def add_messages(username, message):
     """Add messages to the 'messages' list"""
     now = datetime.now().strftime("%H:%M:%S") #%H is the hour in 24h format, %M is for the minutes, %S is for the seconds. The now() method gets the current time
-    messages.append("({}) {}: {}".format(now, username, message)) #displays date, username and then message
-
-def get_all_messages():
-    """Get all of the messages and seperate using a <br> tag"""
-    return "<br>".join(messages)
+    messages_dict = {"timestamp": now, "from": username, "message": message} #messages_dict is a dictionary that stores our now, username and message variables
+    messages.append(messages_dict) #displays date, username and then message
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -28,10 +25,17 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"]) #resends the username each time the page is refreshed
+    
+    return render_template("chat.html", username = username, chat_messages = messages) #the username in chat.html is equal to the username variable in run.py, and the chat_messages in chat.html is equal to the messages list in run.py
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
@@ -39,4 +43,4 @@ def send_message(username, message):
     add_messages(username, message)
     return redirect("/" + username)
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+app.run(host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 5000)), debug=True)
