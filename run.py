26a071bf2115cf32f220bf5964fa73bdf20ed3e9
Ubuntu <ubuntu@ip-172-31-80-83.ec2.internal>
2020-04-02 13:19:38 +0000

    changed debug to false

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from datetime import datetime #imports the datetime module from python's standar
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 def add_message(username, message):
@@ -36,4 +36,4 @@ def user(username):
     
     return render_template("chat.html", username = username, chat_messages = messages) #the username in chat.html is equal to the username variable in run.py, and the chat_messages in chat.html is equal to the messages list in run.py
 
-app.run(host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 5000)), debug=True)
+app.run(host=os.getenv("IP", '0.0.0.0'), port=int(os.getenv("PORT", 5000)), debug=False)
